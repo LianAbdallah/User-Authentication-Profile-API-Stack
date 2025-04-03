691398d524ca0302a2bd2169b08a24dd24e5f175
LianAbdallah <lian.saadeh3@gmail.com>
2025-04-03 22:22:57 +0300

    update

---
+++ project/project/profile.sql
@@ -0,0 +1,31 @@
+use profile; 
+
+create table users (
+usersid int auto_increment Primary key,
+email varchar(255) not null unique, 
+fullName varchar(255) not null,
+userName varchar(255) not null unique,
+password varchar(255) not null,
+phone varchar(255),
+createAt date,
+lastLogin date,
+isAdmin bit DEFAULT 0
+#constraint email_userName_uq unique (email, userName)
+);
+select * from users;
+#add new user - regsiteration 
+INSERT INTO users (email, fullName, userName, password, phone, createAt)
+VALUES ('lian.saadeh3@gamil.com', 'lian saadeh' , 'lsaadeh', '123456', '+9627978021', NOW());
+
+#login base on email or username base on user input
+select * from users where email = 'lian.saadeh3@gamil.com' or userName = 'lsaadeh';
+#then should upadte date if correct login 
+update users set lastLogin = NOW() where email= 'lian.saadeh3@gamil.com';
+
+#update case need more than line base on user update so now i will leave it but this for update full name only
+update users set fullName = 'lian new' where email= 'lian.saadeh3@gamil.com';
+
+#delete user 
+delete from users where email = 'lian.saadeh3@gamil.com' or userName = 'lsaadeh' limit 1;
+
+#drop table users;
